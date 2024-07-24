4abdba533685f82b778e6415b1bd9bfecf3131e5
Romain Gayon <romaing@google.com>
2024-07-24 16:41:42 +0200

    fix codes

--- disco/ball.py
+++ disco/ball.py
@@ -6,14 +6,14 @@ import rpi_rf
 class Ball():
   """Class to talk to a 433Mhz controlled motored disco ball."""
 
-  STOP_CODE = "13281798"
-  START_CODE = "13281786"
+  STOP_CODE = 13281798
+  START_CODE = 13281786
 
   GPIO = 17
   REPEAT = 5
   PULSE_LENGTH = 390
-  PROTOCOL = 'default'
-  LENGTH = 'default'
+  PROTOCOL = 1
+  LENGTH = 24
 
   def __init__(self):
     self.rf = rpi_rf.RFDevice(self.GPIO)
